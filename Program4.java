9a2d2351f3ae12e964040eef2e809bcb39d6c614
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-08-20 15:12:53 -0700

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -1,7 +1,22 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
 
-
+public class Program4
+{
+    public static void main(String [] args){
+        //Declare Variables
+        int a = 475;
+        int b = 821;
+        int c = 369;
+        int d = 562;
+        //Creating new Scanner
+        Scanner numScanner = new Scanner(System.in);
+        
+        //Read User Input
+        System.out.println("The sum of the four numbers is 2227");
+        
+        
+    }
 
 //Paste console output below:
 /*
